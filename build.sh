af04036ea9f6eff1578d08fef867b409c652d1b6
Lakhan Mandloi <lakhan_m@tekditechnologies.com>
2018-05-28 17:40:30 +0530

    update

--- build.sh
+++ build.sh
@@ -1,8 +1,5 @@
 #!/bin/sh
 cd /project/builds
-echo "Container is ->"$AZURE_STORAGE_CONTAINER
-echo "Account is ->"$AZURE_STORAGE_ACCOUNT
-echo "Key is ->"$AZURE_STORAGE_KEY
 bundle exec jekyll build
 echo "Build Successful..!!"
 az storage blob delete-batch --source $AZURE_STORAGE_CONTAINER --account-name $AZURE_STORAGE_ACCOUNT --account-key $AZURE_STORAGE_KEY
